b25b96be3dab4c26c10205c08c2ace3d84010c1d
LIShuning <lishiuning1955142623@outlook.com>
2019-07-05 11:28:54 +0800

    fix bug

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -15,21 +15,27 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     //1.查询名字是*的第一个employee
     Employee findFirstByName(String name);
+
     //2.找出Employee表中第一个姓名包含`*`字符并且薪资大于*的雇员个人信息
-    Employee findFirstByNameLikeAndSalaryIsGreaterThan(String str, Integer salary);
+    Employee findFirstByNameLikeAndSalaryIsGreaterThan(String character,Integer salary);
+
     //3.找出一个薪资最高且公司ID是*的雇员以及该雇员的姓名
-    @Query("select A.name from Employee A where A.salary = (select max(B.salary) from Employee B where B.companyId = ?1)")
+    @Query("select e.name from Employee e where e.salary=(select max(t.salary) from Employee t where t.companyId=?1)")
     String findEmployee(Integer companyId);
-    //4.实现对Employee的分页查询，每页两个数据
+
+    //4.实现对Employee的分页查询，每页两条数据
     Page<Employee> findAll(Pageable pageable);
+
     //5.查找**的所在的公司的公司名称
-    @Query("select c.name from Company c where c.id = (select e.companyId from Employee e where e.name = ?1)")
-    String findComanyNameByEmployeeName(String name);
-    //6.将*的名字改成*,输出这次修改影响的行数
+    @Query("select c.companyName from Company c where c.id = (select e.companyId from Employee e where e.name = ?1)")
+    String findCompanyNameByEmployeeName(String name);
+
     @Modifying
     @Query("update Employee e set e.name = ?2 where e.name = ?1")
     @Transactional
-    Integer updateName(String name, String newName);
+        //6.将*的名字改成*,输出这次修改影响的行数
+    Integer updateName(String name,String newName);
+
     //7.删除姓名是*的employee
     void deleteByName(String name);
 }
