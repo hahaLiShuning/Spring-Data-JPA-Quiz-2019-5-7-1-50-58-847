dfec9dd45a061fdb3a3f491f844e079a9596661e
LIShuning <lishiuning1955142623@outlook.com>
2019-07-05 10:58:02 +0800

    entity finished

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,48 @@
 package com.example.employee.entity;
 
-public class Company {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
+@Entity
+public class Company {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @NotNull
+    private String companyName;
+
+    @NotNull
+    private Integer employeesNumber;
+
+    public Company(@NotNull String companyName, @NotNull Integer employeesNumber) {
+        this.companyName = companyName;
+        this.employeesNumber = employeesNumber;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Integer getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public void setEmployeesNumber(Integer employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
 }
